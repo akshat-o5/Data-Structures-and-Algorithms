b0b42fdbe3ae24e7ec6e160d0fc94c1ddc8ec924
Akshat <a.akshat903@gmail.com>
2022-01-11 10:59:52 +0530

    To Lower Case solution updated

---
+++ Leetcode-solutions/to_lower_case.java
@@ -0,0 +1,22 @@
+// TO LOWER CASE
+// https://leetcode.com/problems/to-lower-case/
+
+
+package com.akshat;
+
+public class to_lower_case {
+
+    public static void main(String[] args) {
+
+        String s = "Hello";
+
+        System.out.println(toLowerCase(s));
+
+    }
+
+    static String toLowerCase(String s) {
+        s=s.toLowerCase();
+        return s;
+    }
+
+}
