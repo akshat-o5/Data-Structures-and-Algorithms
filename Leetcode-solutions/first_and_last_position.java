a5cde712cba97865f6ab4f8d4ece70a310593d50
Akshat Gupta <a.akshat903@gmail.com>
2022-01-23 11:52:29 +0530

    First and Last position of an Element solution updated

---
+++ Leetcode-solutions/first_and_last_position.java
@@ -0,0 +1,53 @@
+// Find First and Last Position of Element in Sorted Array
+// URL: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+package com.akshat;
+
+import java.util.Arrays;
+
+public class first_and_last_position {
+
+    public static void main(String[] args) {
+
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+
+    }
+
+    static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        int start = bs(nums, target, true);
+        int end = bs(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+
+    static int bs(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]){
+                end = mid - 1;
+            }
+            else if (target > nums[mid]){
+                start = mid + 1;
+            }
+            else{
+                ans = mid;
+                if (findStartIndex){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
