7347d7532b1a813ccffc3b9342628fa2d84ced8c
Akshat Gupta <a.akshat903@gmail.com>
2022-06-04 18:47:26 +0530

    Hashset added

---
+++ collection_frameworks/LearnHashSet.java
@@ -0,0 +1,36 @@
+package com.Rhythm.Collection_Framework_in_one;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LearnHashSet {
+
+    public static void main(String[] args) {
+
+//        Set<Integer> set = new HashSet<>();
+//
+//        set.add(32);
+//        set.add(2);
+//        set.add(54);
+//        set.add(21);
+//        set.add(65);
+//
+//        System.out.println(set);
+//
+//        set.remove(54);
+//        System.out.println(set);
+//
+//        System.out.println(set.contains(21));
+//        System.out.println(set.contains(100));
+//
+//        System.out.println(set.isEmpty());
+//
+//        System.out.println(set.size());
+//
+//        set.clear();
+//        System.out.println(set);
+
+
+    }
+
+}
