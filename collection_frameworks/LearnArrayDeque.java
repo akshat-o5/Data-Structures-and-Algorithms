fa701d9b4db7df219abcfd1afee21656baf8766d
Akshat Gupta <a.akshat903@gmail.com>
2022-06-04 18:46:24 +0530

    ArrayDeque added

---
+++ collection_frameworks/LearnArrayDeque.java
@@ -0,0 +1,33 @@
+package com.Rhythm.Collection_Framework_in_one;
+
+import java.util.ArrayDeque;
+
+public class LearnArrayDeque {
+
+    public static void main(String[] args) {
+
+        ArrayDeque<Integer> adq = new ArrayDeque<>();
+
+        adq.offer(23); // adds to last by default
+        adq.offerFirst(12);
+        adq.offerLast(45);
+
+        System.out.println(adq);
+
+        System.out.println(adq.peek());
+        System.out.println(adq.peekFirst());
+        System.out.println(adq.peekLast());
+
+        System.out.println(adq.poll());
+        System.out.println("poll() " + adq);
+
+        System.out.println(adq.pollFirst());
+        System.out.println("pollFirst() " + adq);
+
+        System.out.println(adq.pollLast());
+        System.out.println("pollLast() " + adq);
+
+
+    }
+
+}
