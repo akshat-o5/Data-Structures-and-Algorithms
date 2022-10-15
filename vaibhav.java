8cecdbfb0b51a37ba3f283a33033f09c867a59c2
Vaibhav Malkar <93985798+vaibhvmalkar@users.noreply.github.com>
2022-10-15 18:44:02 +0530

    Create vaibhav.java

---
+++ vaibhav.java
@@ -0,0 +1,13 @@
+class IfStatement {
+  public static void main(String[] args) {
+
+    int number = 10;
+
+    // checks if number is less than 0
+    if (number < 0) {
+      System.out.println("The number is negative.");
+    }
+
+    System.out.println("Statement outside if block");
+  }
+}
