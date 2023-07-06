0f1d7952f06b317ec2a3a53fb6ae71734ca54555
Akshat Gupta <96832359+akshat-o5@users.noreply.github.com>
2023-07-07 00:36:59 +0530

    Create floor.cpp

---
+++ Practice-questions/floor.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int floor(vector<int> vec, int n){
+    int start = 0;
+    int end = vec.size() - 1;
+    int ans = -1;
+    while(start<=end){
+        int mid = start+(end-start)/2;
+        if(vec[mid]<=n){
+            ans = vec[mid];
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+
+int main() {
+    // Write C++ code here
+    // std::cout << "Hello world!";
+    
+    vector<int> vec {3, 5, 7, 9, 11, 13};
+    
+    cout<<floor(vec, 6)<<endl;
+    
+    return 0;
+}
