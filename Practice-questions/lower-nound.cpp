bdd18e93b45111d10c0bfd537fcc32f35e874219
Akshat Gupta <96832359+akshat-o5@users.noreply.github.com>
2023-07-07 00:17:58 +0530

    Create lower-nound.cpp

---
+++ Practice-questions/lower-nound.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int lb(vector<int> vec, int n){
+    int start = 0;
+    int end = vec.size() - 1;
+    int ans = vec.size();
+    while(start<=end){
+        int mid = start+(end-start)/2;
+        if(vec[mid]>=n){
+            ans = mid;
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
+
+
+int main() {
+    // Write C++ code here
+    // std::cout << "Hello world!";
+    
+    vector<int> vec {3, 5, 7, 9, 11, 13};
+    
+    cout<<lb(vec, 1)<<endl;
+    
+    return 0;
+}
