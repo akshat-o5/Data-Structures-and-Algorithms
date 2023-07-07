5e38832ad5432af98fb3852fb3159640221c06bd
Akshat Gupta <96832359+akshat-o5@users.noreply.github.com>
2023-07-07 15:37:00 +0530

    Create Binary-Search.cpp

---
+++ binary search/Binary-Search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start<=end){
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]){
+                end = mid - 1;
+            }
+            else if (target > nums[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
