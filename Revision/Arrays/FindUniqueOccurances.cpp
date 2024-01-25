44e97a0d473c7e9a087ffe7116380c2b8d6c51ed
Akshat Gupta <a.akshat903@gmail.com>
2024-01-25 22:22:31 +0530

    files added

---
+++ Revision/Arrays/FindUniqueOccurances.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/unique-number-of-occurrences/
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+          int n = arr.size();
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            m[arr[i]]++;
+        }
+        set<int> s;
+        for(auto a: m){
+            s.insert(a.second);
+        }
+        if(s.size() == m.size()){
+            return true;
+        }
+        return false;
+    }
+};
