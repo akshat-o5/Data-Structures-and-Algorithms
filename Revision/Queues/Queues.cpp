bbb6dbb2d29918e105ca53415791cf568ca5e40a
Akshat Gupta <a.akshat903@gmail.com>
2024-02-22 13:18:30 +0530

    files added

---
+++ Revision/Queues/Queues.cpp
@@ -0,0 +1,103 @@
+// https://www.geeksforgeeks.org/problems/implement-queue-using-array/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+};
+
+class MyQueue {
+private:
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+
+// } Driver Code Ends
+
+
+/* 
+
+The structure of the class is
+class MyQueue {
+private:
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+ */
+
+//Function to push an element x in a queue.
+void MyQueue :: push(int x)
+{
+        // Your Code
+        if(rear == 100005){
+            return;
+        }
+        else{
+            arr[rear] = x;
+            rear++;
+        }
+        
+}
+
+//Function to pop an element from queue and return that element.
+int MyQueue :: pop()
+{
+        // Your Code
+        if(front == rear){
+            return -1;
+        }
+        int ans = arr[front];
+        arr[front] = -1;
+        front++;
+        if(front == rear){
+            front = 0;
+            rear = 0;
+        }
+        return ans;
+        
+}
