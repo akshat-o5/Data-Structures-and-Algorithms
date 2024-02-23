b4b21b7372aa879566e0ffa9a915aae400c5b825
Akshat Gupta <a.akshat903@gmail.com>
2024-02-23 13:20:26 +0530

    files added

---
+++ Revision/SDE SHEET/NextLargerElement.cpp
@@ -0,0 +1,54 @@
+// https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long> ans(n, -1);
+        stack<long long> st;
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && st.top() <= arr[i]) {
+                st.pop();
+            }
+            if(st.empty() == false) ans[i] = st.top();
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
