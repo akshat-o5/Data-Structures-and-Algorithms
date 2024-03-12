4834e9f296a4afdb9b340c6cadfa212cb0e199f8
Akshat Gupta <a.akshat903@gmail.com>
2024-03-12 17:21:39 +0530

    files added

--- Revision/DP/NthFibonacci.cpp
+++ Revision/DP/NthFibonacci.cpp
@@ -42,6 +42,22 @@ public:
         return vec[n];
     }
     // Using Bottom Up Approach Ends
+
+    // Using Space Optimization Starts
+    int nthFibonacci(int n){
+        int prev1=1;
+        int prev2=0;
+        if(n==0){
+            return 0;
+        }
+        for(int i=2; i<=n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+    // Using Space Optimization Ends
 };
 
 //{ Driver Code Starts.
