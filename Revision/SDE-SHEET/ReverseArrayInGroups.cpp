af401df2ce894fa9fce57555372e814df3f3d452
Akshat Gupta <a.akshat903@gmail.com>
2024-05-08 13:13:57 +0530

    files added

---
+++ Revision/SDE-SHEET/ReverseArrayInGroups.cpp
@@ -0,0 +1,59 @@
+// https://www.geeksforgeeks.org/problems/reverse-array-in-groups0255/1
+
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    //Function to reverse every sub-array group of size k.
+    void reverseInGroups(vector<long long>& arr, int n, int k){
+        // code here
+        for(int i=0; i<n; i=i+k){
+            if(i+k>n){
+                reverse(arr.begin()+i, arr.end());
+            }
+            else{
+                reverse(arr.begin()+i, arr.begin()+i+k);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t; 
+    cin >> t; 
+    while(t--){ 
+        int n;
+        cin >> n; 
+        vector<long long> arr; 
+        int k;
+        cin >> k; 
+
+        for(long long i = 0; i<n; i++)
+        {
+            long long x;
+            cin >> x; 
+            arr.push_back(x); 
+        }
+        Solution ob;
+        ob.reverseInGroups(arr, n, k);
+        
+        for(long long i = 0; i<n; i++){
+            cout << arr[i] << " "; 
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
