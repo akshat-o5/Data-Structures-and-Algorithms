77a1da194282c58028afcd1047e7b321a19f2974
Akshat Gupta <a.akshat903@gmail.com>
2024-05-11 17:10:35 +0530

    files added

---
+++ Revision/Sets/UnionOf2SortedArrays.cpp
@@ -0,0 +1,69 @@
+// https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1?page=4&difficulty=Medium&sortBy=submissions
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        //Your code here
+        //return vector with correct order of elements
+          set<int> s;
+        
+        for(int i =0;i<n;i++)
+        {
+            s.insert(arr1[i]);
+        }
+        
+        for(int i =0;i<m;i++)
+        {
+            s.insert(arr2[i]);
+        }
+        
+        vector<int> v(s.begin(), s.end());
+        return v;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
