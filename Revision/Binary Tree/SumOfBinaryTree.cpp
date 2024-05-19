3778333686782acb1b0b132da28bbae134dd6ab8
Akshat Gupta <a.akshat903@gmail.com>
2024-05-19 23:52:51 +0530

    files added

---
+++ Revision/Binary Tree/SumOfBinaryTree.cpp
@@ -0,0 +1,103 @@
+// https://www.geeksforgeeks.org/problems/sum-of-binary-tree/1?page=1&category=Tree&difficulty=Basic&sortBy=submissions
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+Node* make_tree()
+{
+	int n;
+	cin>>n;
+    
+	Node* head=NULL;
+	queue <Node*> q;
+
+	for( ; n>0 ; n-=2 )
+	{
+		int a,b;
+		char c;
+		cin>> a >> b >> c;
+
+        if(!head)
+		{
+			head = new Node(a);
+			q.push(head);
+		}
+
+        Node* pick = q.front();
+		q.pop();
+
+		if(c == 'L')
+		{
+			pick->left = new Node(b);
+			q.push( pick->left );
+		}
+		
+		
+		cin>> a >> b >> c;
+		
+		if(c == 'R')
+		{
+			pick->right = new Node(b);
+			q.push( pick->right );
+		}
+	}
+	return head;
+}
+
+long int sumBT(Node* root);
+
+
+int main()
+{
+    int t;cin>>t;while(t--){
+    	
+    	Node* head = make_tree();
+    
+        cout<<sumBT(head)<<endl;
+    }
+	return 1;
+}
+
+// } Driver Code Ends
+
+
+/*Structure of the node of the binary tree
+struct Node
+{
+	int key;
+	Node* left, *right;
+};
+*/
+// Function should return the sum of all the elements
+// of the binary tree
+
+void solve(Node* root, long int &sum){
+    if(root == NULL){
+        return;
+    }
+    solve(root->left, sum);
+    sum = sum + root->key;
+    solve(root->right, sum);
+}
+
+long int sumBT(Node* root)
+{
+    // Code here
+    long int sum = 0;
+    solve(root, sum);
+    return sum;
+}
